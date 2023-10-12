f5a480d76da9b4fede192b469b7b72ca081eb660
Naitik Patel <naitikpatel107@gmail.com>
2023-10-12 18:51:47 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -18,30 +18,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.use("/api/product", prodcutRoutes);
-// if (process.env.NODE_ENV === "production") {
-//   const __dirname = path.resolve();
-//   console.log('__dirname:', __dirname);
-//   const indexPath = path.join(__dirname, "frontend", "dist", "index.html");
-//   console.log('indexPath:', indexPath);
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend")));
 
-//   app.use(express.static(path.join(__dirname, "frontend/dist")));
-
-//   app.get("*", (req, res) => res.sendFile(indexPath));
-// }
-
-// if (process.env.NODE_ENV === "production") {
-//   const __dirname = path.resolve();
-//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-//   );
-// }
-// else {
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => console.log(`Server is running on ${port}...`));
